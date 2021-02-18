4c38ae1de4d95710f77a48a7ca620cc4b2c18765
Malbina <malbinaramilova@gmail.com>
2021-02-18 11:40:52 +0100

    27 gemacht!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,39 @@
 package com.company;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World, I'm Malbina ");
+    public static void main(String[] args) throws IOException {
+
+           FileReader fr = new FileReader("songSia.txt");
+           Scanner sc = new Scanner(fr);
+           for (int i = 1; sc.hasNextLine(); i++) {
+               System.out.println(sc.nextLine());
+           }
+           fr.close();
+
+        FileWriter fw = new FileWriter("CheapThrills.txt");
+        fw.write("Исполнитель: Sia\n");
+        fw.write("Название песни: Cheap Thrills\n");
+        fw.write("Жанр песни: Pop\n");
+        fw.write("Tекст песни:\n" +
+                "Come on, come on, turn the radio on\n" +
+                "It's Friday night, and it won't be long\n" +
+                "Gotta do my hair, put my make-up on\n" +
+                "It's Friday night, and it won't be long\n" +
+                "'Til I hit the dance floor, hit the dance floor\n" +
+                "I got all I need");
+
+
+        fw.close();
+
+
+
+
+
     }
 }
